e698d7dd03c06fbb0a30256d54862d7e4df61a63
gopinadh3541 <39827201+gopinadh3541@users.noreply.github.com>
2018-10-25 18:38:49 +0530

    Update BankApplication.java

--- src/main/java/com/capgemini/ui/BankApplication.java
+++ src/main/java/com/capgemini/ui/BankApplication.java
@@ -87,6 +87,12 @@ public class BankApplication extends HttpServlet {
 		String pwd = request.getParameter("pwd");
 
 		if (uname.equals(pwd) && uname != "") {
+			System.out.println("Password is: "+pwd);
+			byte[] encrypt =Base64.getEncoder().encode(pwd.getBytes());
+			System.out.println("Encrypted text is: "+ new String(encrypt));
+			
+			byte[] decrypt=Base64.getDecoder().decode(encrypt);
+			System.out.println("Decrypted text is: "+ new String(decrypt));
 			response.sendRedirect("menu.jsp");
 		} else {
 			HttpSession session = request.getSession();
